3799f9a80ab699e60621da064b932755b9cc9c46
jimin <a@b.com>
2026-04-03 12:21:07 +0900

    homecontroller

--- src/main/java/com/kedu/controllers/HomeController.java
+++ src/main/java/com/kedu/controllers/HomeController.java
@@ -1,14 +1,23 @@
 package com.kedu.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kedu.dao.WorkPlaceDAO;
+import com.kedu.dao.WorkPlaceListDAO;
+
 @Controller
 public class HomeController {
-
+	@Autowired
+	public WorkPlaceDAO wpdao;
+	
+	@Autowired
+	public WorkPlaceListDAO wpldao;
+	
 	@RequestMapping("/")
 	public String home() {
-	return "home";	
+		return "home";	
 	}
 }
 	
