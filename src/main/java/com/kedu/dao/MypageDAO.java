8d16a2e946e30359a6b92d773e86d7cd0b00fa98
jimin <a@b.com>
2026-04-08 20:48:30 +0900

    mypagedao

--- src/main/java/com/kedu/dao/MypageDAO.java
+++ src/main/java/com/kedu/dao/MypageDAO.java
@@ -47,4 +47,9 @@ public class MypageDAO {
 		    
 		    return jdbc.query(sql, new BeanPropertyRowMapper<JobPostDTO>(JobPostDTO.class), parentId);
 		}
+		
+//		//이력서 저장
+//		public void insert_resume() {
+//			String sql = "insert into resume values()";
+//		}
 }
